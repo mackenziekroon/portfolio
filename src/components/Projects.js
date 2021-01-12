cc31540f923c408512882177ef882a6480faf05c
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-01-12 17:55:13 -0600

    added seekr img

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,15 @@
 import React from "react";
+import match from "../match.png";
 
 const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
-      <h3>seekr</h3>
+      <div className="seekr-container">
+        <h3>seekr</h3>
+        <img className="seekr-img" alt="seekr-img" src={match} />
+      </div>
+
       <h3>Pup Quiz</h3>
       <h3>Fruitify Juicery</h3>
     </div>
